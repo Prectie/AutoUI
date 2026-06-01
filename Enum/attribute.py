36cd3e49eca7bdce752d9ec5f72268dbc4c0f809
»ÆÌìÓî <820733450@qq.com>
2026-06-02 00:24:17 +0800

    删除selenium框架改为playwright

--- Enum/attribute.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Attribute(Enum):
-    """ 存放 HTML 标签里的各种属性名, property 与 attribute 均适用 """
-    CLASS = 'class'
-    STYLE = 'style'
-    WIDTH = 'width'
-    HEIGHT = 'height'
--- Enum/url.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class URLEnum(Enum):
-    """ 涉及到的 url"""
-    ORP_LOGIN_URL = 'https://www.bing.com/search?q=%e7%99%be%e5%ba%a6%e8%b4%b4%e5%90%a7&qs=HS&pq=%e7%99%be%e5%ba%a6&sc=11-2&cvid=8E75791699774A348B2A2A3997A59DAE&FORM=QBRE&sp=1&lq=0'
-
-    ORP_MONITOR = 'http://192.168.1.141:28091/#/'
-
